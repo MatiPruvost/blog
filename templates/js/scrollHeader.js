dcc6864b570a4c03ff10e05264387ce7e7a490c6
MatiPruvost <matiaspruvost@gmail.com>
2015-11-15 19:19:37 -0300

    Fix margin top on content

--- templates/js/scrollHeader.js
+++ templates/js/scrollHeader.js
@@ -4,13 +4,15 @@ $(function() {
     if (scroll >= 10) {
       $(".header").addClass('smaller');
       $("#content").addClass('smaller-content');
-      /*var headerHeight = $('.header').height();
-      console.log(headerHeight)
-      $(".smaller-content").css("margin-top", headerHeight+"px");*/
+      var headerHeight = $('.header').height();
+      var headerMarginBottom = parseInt($(".header").css("margin-bottom"),10);
+      var headerMarginTop = parseInt($(".header").css("margin-top"),10);
+      var contentMarginTop = headerHeight + headerMarginBottom;
+      $("#content").css("margin-top", contentMarginTop+"px");
     } else {
       $(".header").removeClass("smaller");
       $("#content").removeClass("smaller-content");
-      /*$(".smaller-content").css("margin-top", "inherit");*/
+      $("#content").css("margin-top", "inherit");
     }
   });
 });
